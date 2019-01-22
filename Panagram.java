40b96f30f325ad27e38fea9f5ca8c771681bbe3c
Rishabh Kumar <rishthegupta@gmail.com>
2019-01-22 20:10:19 +0530

    Update Panagram.java

--- Panagram.java
+++ Panagram.java
@@ -1,23 +1,38 @@
 import java.util.Arrays;
 
-public class Panagram {
+public class Pangram {
 	
 	
 	
-	static void check(String s)
+	static void check(String str)
 	{
-		s=s.toLowerCase();
 		
-		char[] ip=s.toCharArray();
 		
-		int i, len=s.length();
-		int[] cap=new int[26];
-		Arrays.sort(ip);
 		
-		for(i=0;i<len;i++)
+		
+		int i, len=str.length(),index=0;
+		int[] flag=new int[26];
+		for(i=0;i<str.length();i++)
 		{
-			cap[(int)ip[i]-97]++;
+			
+		
+			if ('A' <= str.charAt(i) &&  str.charAt(i) <= 'Z') 
+	                      
+	            index = str.charAt(i) - 'A'; 
+	
+	        else if('a' <= str.charAt(i) && str.charAt(i) <= 'z') 
+	                          
+	            index = str.charAt(i) - 'a'; 
+			flag[index]=1;
 		}
+		
+		for(i=0;i<26;i++)
+		{
+			if(flag[i]==0)
+				System.out.println("not Pangram");
+				
+		}
+		System.out.println("Pangram");
 	}
 
 	public static void main(String[] args) {
